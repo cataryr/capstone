86a865b91099c4daa3d761aa3644e326efdab769
NicolasMorenoC <81115006+NicolasMorenoC@users.noreply.github.com>
2024-08-23 19:28:52 -0400

    Delete MagikStock/DISEÑO1.1 directory

--- MagikStock/DISEÑO1.1/js/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("toggle-btn").addEventListener("click", function() {
-    var sidebar = document.getElementById("sidebar-wrapper");
-    sidebar.classList.toggle("collapsed");
-  });  
